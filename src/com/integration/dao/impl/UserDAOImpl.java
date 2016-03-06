6b9b55b6aa6c438dba2f54aa24815c927574360e
hanwen <1106002609@qq.com>
2016-03-06 15:05:33 +0800

    对UserDAO进行实现

---
+++ src/com/integration/dao/impl/UserDAOImpl.java
@@ -0,0 +1,47 @@
+package com.integration.dao.impl;
+
+import java.util.List;
+
+import com.integration.dao.UserDAO;
+import com.integration.entity.User;
+
+public class UserDAOImpl implements UserDAO {
+
+	//添加用户
+	@Override
+	public void save(User user) {
+		// TODO Auto-generated method stub
+
+	}
+	//按用户名查找用户
+	@Override
+	public User getUser(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	//删除用户
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+
+	}
+	//更新用户
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+
+	}
+	//按id查找用户
+	@Override
+	public User findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	//查找全部用户
+	@Override
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
